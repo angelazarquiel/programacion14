c5890c231e68fdef8d400ff67dfaade5a991dd64
usuario <usuario@B301-PROFE.dai.red>
2014-10-27 17:56:34 +0100

    27/10

---
+++ UT2_Intro/src/ejercicios/dia22/E0218.java
@@ -0,0 +1,25 @@
+package ejercicios.dia22;
+
+public class E0218 {
+
+	public static void main(String[] args) {
+		String cuadrado1="*********"
+			  ,cuadrado2="*       *";
+		
+		System.out.println(cuadrado1);
+		System.out.println(cuadrado2);
+		System.out.println(cuadrado2);
+		System.out.println(cuadrado1);
+		
+		System.out.printf("%s\n%s\n%s\n%s\n%s\n%s",
+				cuadrado1,
+				cuadrado2,
+				cuadrado2,
+				cuadrado2,
+				cuadrado2,
+				cuadrado1
+				);
+
+	}
+
+}
