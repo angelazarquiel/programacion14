c72e43ac9d3e2a0ece839f7fed6196235998ed97
usuario <usuario@B301-PROFE.dai.red>
2015-03-20 19:51:59 +0100

    20

--- UT8Clases/src/ejercicios/bloque3/PruebaComplejo.java
+++ UT8Clases/src/ejercicios/bloque3/PruebaComplejo.java
@@ -1,5 +1,8 @@
 package ejercicios.bloque3;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class PruebaComplejo {
 
 	public static void main(String[] args) {
@@ -29,6 +32,50 @@ public class PruebaComplejo {
 		
 		uno.divide(dos);
 		assert uno.equals(new Complejo(4,-1));
+		
+		pruebaGenerador();
+		
+		array();
+		
+	}
+	
+	private static void array() {
+		ArrayList<Complejo> numeros;
+		
+		numeros=new ArrayList<Complejo>();
+		
+		for(int i=0;i<10;i++)
+			numeros.add(new Complejo(numero(-10,10),numero(-10,10)));
+		
+		//System.out.println(numeros);
+		for(Complejo c : numeros)
+			System.out.println(c);
+		
+		Complejo dos=new Complejo(2,0);
+		
+		for(Complejo c : numeros)
+			c.multiplica(dos);
+		
+		System.out.println("Multiplicado por 2");
+		for(Complejo c : numeros)
+			System.out.println(c);
+	}
+
+	public static int numero(int min,int max) {
+		int aleatorio;
+		
+		aleatorio=(int)(Math.random()*(max-(min)+2)+min-1);
+		
+		return aleatorio;
+	}
+	
+	static void pruebaGenerador() {
+		int[] resultados=new int[21];
+		
+		for(int i=0;i<1000000;i++)
+			resultados[numero(-10,10)+10]++;
+		
+		System.out.println(Arrays.toString(resultados));
 		
 	}
 
